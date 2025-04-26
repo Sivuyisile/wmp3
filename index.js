52d224ffce90123b5abc2be199cada909c2d1d0b
Sivuyisile <sivuyisilenkosi86@gmail.com>
2025-04-26 19:33:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,148 @@
+﻿$(documennt).ready(wmp index.html){
+    var=background,foreground1,foreground2,foreground3,header1,header2,header3,header4,p1,p2,p3,navBar,select box,foregroundVideo,searchBar,logoImage,toggleSlider,shoppingNavCart,bluePill,parallex,emP,costOfItems,coutureImages,addToCart;
+    
+    background=$(document).css(background:HEX:F5F5F5; 
+        rgb:(245,245,245););
+        foreground1=$(foreground).sibling(images\index foreground img.gif.jpeg.png);
+        foreground2=$(foreground).sibling(images\men foreground img.gif.jpeg.png);
+        foreground3=$(foreground).sibling(images\women foreground img.gif,jpeg.png);
+        foregroundVideo=$(foreground).index(0)replaceWith(images\foreground video.mp4)on(click(toggleSlider)function(e){},false);
+        navBar=$(ul#navBar).class(navBar).children(selectBox,searchBar,logoImage,toggleSlider,shoppingNavBar);
+        header1=$(h1).class(fontFamily:helvetica);
+        header2=$(h2).class(fontFamily:helvetica);
+        header3=$(h3).class(fontFamily:helvetica);
+        header4=$(h4).class(fontFamily:helvetica);
+        p1=$(p1).class(fontFamily:helvetica);
+        p2=$(p2).class(fontFamily:helvetica);
+        p3=$(p3).class(fontFamily:helvetica);
+        parallex=$(document#body).hidden()on(.scrollRight,function(e){.fadein(100ms).slidedown()
+          
+        })var bluePill=$(li:contain(home latest))on(.click,function(e){
+            target=e.target||e.scrElement;
+            e.preventDefalt();
+            $(document).unload(index\home latest.html)false;
+           
+        })
+
+      //  js for the cart button
+      
+      document.querySelectorall("cart button")
+    cartButtons.forEach(button=>{button.addEventListener("click",cartClick);});
+function cartclick(){let button=this;button.classList.add('clicked');}    
+
+//js for the slide 3d 
+
+let box=document.querySelector("wrapper");
+    let cards=box.querySeletorAll("span");
+    let n=cards.length;
+    let rotationDegree+360/n;
+    let boxSize=Math.max(10,constant/n)+"vw";
+    box.style.width=box.style.height=boxSize;
+    cards.forEach((card.index)=>{
+    card.style.setProperty("--n",rotationDegree+"deg");});
+
+    letviewportWidth90=window.innerWidth*0.9;
+    let distanceFactor=viewportWidth90/360;
+
+
+    window.addEventListener("resize",()=>{
+        viewportWidth90=window.innerWidth*0.9;
+        distanceFactor=viewportWidth/360;
+    });
+    
+    window.onmousemove=function(e){
+    let x=e.clientX/distanceFactor;
+    box.style.transform="perspective("+ viewportWidth90+px")rotateY(+ x +"deg");};
+    </js>
+            coutureImages=$(li#wrapperSpanStyle).class(flex-wrapper,img).append(emP,costOfItems,addToCart);
+            emP==("");
+            emp=<p>+ informationOfProduct +</p>;
+            informationOfProduct=$(emp).text();
+            costOfItems=$(costOfItems).isNumeric().data(price);
+            coutureImages=$(li#wrapperSpanStyle).each()on(.click,function(e){document. 
+                getElementByID(wrapperSpanStyle).append(#body)
+             e.target||e.src.element;
+              var embedImg=document.getElementByID(wrapperSpanStyle).append(#body);
+              document.innerhtml=$(#body:not(embedImg)).blur();
+          });  
+            addToCart=$(addToCart).each().class(fas-fa shopping cart plus)on(.click slideDown,function(e){e.target||e.scrElement;
+                .addclass(fas-fa shopping cart)
+            })
+        updateCount();
+        },false;
+    e.preventDefault();
+    updateCount(e){
+        e.target||e.srcElement;
+        shoppingNavCart=i,i!=0;i++function(e){
+            $(addToCart)on.click
+        }else{$(addToCart)on.dbclick,function(e){shopping-=i;}}
+    };
+    var quantity=i,i=o;functiom(){
+        for .each()on(click($add to cart)).getParentAttr(wrapperSpanStyle).class(flex-wrapper,img) && i++;function(){
+    document.innerhtml=elementSetAttrById(wrapperSpanStyle).children(imformatiionOfProduct).table data();
+    document.innerhtml=elementSetAttrById(quantity).table data();
+    document.innerhtml=elementSetAttrById(wrapperSpanStyle).children(costOfItem).table data(price);
+    document.innerhtml=elementSetAttrById(#totalSum).table data(price);
+     }else
+     { 
+        for .each()on(dbclick($add to cart)).removeParentAttr(wrapperSpanStyle).class(flex-wrapper,img) && i--;
+        function(){
+        document.innerhtml=elementRemoveAttrById(wrapperSpanStyle).children(imformatiionOfProduct).table data();
+        document.innerhtml=elementRemoveAttrById(quantity).table data();
+        document.innerhtml=elementRemoveAttrById(wrapperSpanStyle).children(costOfItem).table data(price);
+        document.innerhtml=elementRemoveAttrById(#totalSum).table data(price);
+       }}};
+
+var play=$(play:button).class(ai ai play);
+type=e.type;
+e.type=$(toggleSlider)on(.click,function(e){
+    videoControls(e);
+    audioControls(e);
+},false);
+function videoControls(e){
+    target=e.target||event.scrElement;
+    e.preventDefalt();
+    switch(target.getAttr(data-state)){
+        case "play":play(target);
+        break;
+        case"stop":stop(target);
+        break;
+        case "soundOn":soundOn(target);
+        break;
+        case "soundOff":soundOff(target);
+        break;
+    }
+};
+function play(target) {target.setAttr(data-state;play);
+    target.textContent="play";
+};
+function stop(target) {target.setAttr(data-state;stop);
+    target.textContent="play";
+};function sounOn(target) {target.setAttr(data-state;sounOn);
+    target.textContent="soundOn";
+};
+function soundOff(target) {target.setAttr(data-state;soundOff);
+    target.textContent="soundOff";
+}; 
+var menCouture=$(men:button).class(btn btn-menCouture).innerHTML(index\men couture.php);
+var womenCouture=$(women:button).class(btn btn-womenCouture).innerHTML(index\women couture.php);
+
+      footer=$(li:contain(option value)).innerHTML();
+    var this=$(this);
+    footer=$(document).appendchildren(services.html,follow us.html);
+    footer=$(this).class(a:button);
+    this=$(this).each()on(.click,function(e){
+        $(document).unload()});
+    selectbox=$(ul:selectBox).chilren(hom latest,events.html,registration.html,contactUS.html);
+    var this=$(this).each()on(.click,function(e){
+        $(document).unload()});
+    logoImage=$(img:contain(logoImage)).class(img)
+    var feedback=('');
+    searchBar=$(searchBar:input).text().val(),fuction(e){
+        e.target||e.scrElement;
+        e.type=$(searchBar:input)on(.submit,getAttr(){
+            e.preventDefault();
+        }else{
+            feedback=$(document).show("Unable to find what you are looking for....do try again")
+        })
+    }}
