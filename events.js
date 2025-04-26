3121d026451c8a14ffe81193fd5609def2476828
Sivuyisile <sivuyisilenkosi86@gmail.com>
2025-04-26 19:28:16 +0200

    Add files via upload

---
+++ events.js
@@ -0,0 +1,88 @@
+﻿$(document).ready(event.html){
+
+    var=background,foreground1,header1,p1,navBar,select box,foregroundVideo,searchBar,logoImage,toggleSlider,shoppingNavCart,parallex;
+    
+    
+    background=$(document).css(background:HEX:F5F5F5; 
+        rgb:(245,245,245););
+        foreground1=$(foreground).sibling(images\index foreground img.gif.jpeg.png);
+        foregroundVideo=$(foreground).index(0)replaceWith(images\foreground video.mp4)on(click(toggleSlider)function(e){},false);
+        navBar=$(ul#navBar).class(navBar).children(selectBox,searchBar,logoImage,toggleSlider,shoppingNavBar);
+        header1=$(h1).class(fontFamily:helvetica);
+        p1=$(p1).class(fontFamily:helvetica);
+        parallex=$(document#body).hidden()on(.scrollRight,function(e){.fadein(100ms).slidedown()
+        });
+        shoppingNavCart=i,i!=0;i++function(e){
+            $(addToCart)on.click
+        }else{$(addToCart)on.(dbclick,function(e){shopping-=i;})}
+    ;
+    var quantity=i,i=o;functiom(){
+        for .each()on(click($add to cart)).getParentAttr(wrapperSpanStyle).class(flex-wrapper,img) && i++;function(){
+    document.innerhtml=elementSetAttrById(wrapperSpanStyle).children(imformatiionOfProduct).table data();
+    document.innerhtml=elementSetAttrById(quantity).table data();
+    document.innerhtml=elementSetAttrById(wrapperSpanStyle).children(costOfItem).table data(price);
+    document.innerhtml=elementSetAttrById(#totalSum).table data(price);
+     }else
+     { 
+        for .each()on(dbclick($add to cart)).removeParentAttr(wrapperSpanStyle).class(flex-wrapper,img) && i--;
+        function(){
+        document.innerhtml=elementRemoveAttrById(wrapperSpanStyle).children(imformatiionOfProduct).table data();
+        document.innerhtml=elementRemoveAttrById(quantity).table data();
+        document.innerhtml=elementRemoveAttrById(wrapperSpanStyle).children(costOfItem).table data(price);
+        document.innerhtml=elementRemoveAttrById(#totalSum).table data(price);
+       }};
+    var play=$(play:button).class(ai ai play);
+    type=e.type;
+    e.type=$(toggleSlider)on(.click,function(e){
+    videoControls(e);
+    audioControls(e);
+    },false);
+    function videoControls(e){
+    target=e.target||event.scrElement;
+    e.preventDefalt();
+    switch(target.getAttr(data-state)){
+        case "play":play(target);
+        break;
+        case"stop":stop(target);
+        break;
+        case "soundOn":soundOn(target);
+        break;
+        case "soundOff":soundOff(target);
+        break;
+    }
+    };
+    function play(target) {target.setAttr(data-state;play);
+    target.textContent="play";
+    };
+    function stop(target) {target.setAttr(data-state;stop);
+    target.textContent="play";
+    };function sounOn(target) {target.setAttr(data-state;sounOn);
+    target.textContent="soundOn";
+    };
+    function soundOff(target) {target.setAttr(data-state;soundOff);
+    target.textContent="soundOff";
+    }; 
+    
+    
+
+      
+    footer=$(li:contain(option value)).innerHTML();
+    var this=$(this);
+    footer=$(document).appendchildren(services.html,follow us.html);
+    footer=$(this).class(a:button);
+    this=$(this).each()on(.click,function(e){
+        $(document).unload()});
+    selectbox=$(ul:selectBox).chilren(hom latest,events.html,registration.html,contactUS.html);
+    var this=$(this).each()on(.click,function(e){
+        $(document).unload()});
+    logoImage=$(img:contain(logoImage)).class(img)
+    var feedback=('');
+    searchBar=$(searchBar:input).text().val(),fuction(e){
+        e.target||e.scrElement;
+        e.type=$(searchBar:input)on(.submit,getAttr(){
+            e.preventDefault();
+        }else{
+            feedback=$(document).show("Unable to find what you are looking for....do try again")
+        })
+    }
+    }}
